d967f17cbb0be97402f3ee6a722b9000f21eb443
Jay Pedregosa <alexanderj.pedregosa@gmail.com>
2020-09-21 18:10:55 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(sentence)
   sentence = sentence.split(" ")
-  reversed_sentence = new_sentence.each {|x| x.reverse!}
-  return reversed_array.join(" ")
+  reversed_sentence = new_sentence.each {|word| word.reverse!}
+  return reversed_sentence.join(" ")
 end
