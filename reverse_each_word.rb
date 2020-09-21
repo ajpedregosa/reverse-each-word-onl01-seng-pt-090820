91a0c411380558aa09e7e43fa3472262a2ffb211
Jay Pedregosa <alexanderj.pedregosa@gmail.com>
2020-09-21 18:13:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,3 +3,9 @@ def reverse_each_word(sentence)
   reversed_sentence = new_sentence.each {|word| word.reverse!}
   return reversed_sentence.join(" ")
 end
+
+def reverse_each_word(sentence)
+  new_sentence = sentence.split(" ")
+  reversed_sentence = new_sentence.collect {|word| word.reverse!}
+  return reversed_sentence.join(" ")
+end
